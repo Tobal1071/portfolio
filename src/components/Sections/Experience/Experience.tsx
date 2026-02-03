5d46400207352cbac7a0cebc1f0e1ae30d42bbb1
Tobal1071 <cristobalamat.ca@gmail.com>
2026-02-03 11:44:58 +0100

    Cambios experiencia

--- src/components/Sections/Experience/Experience.tsx
+++ src/components/Sections/Experience/Experience.tsx
@@ -9,7 +9,7 @@ function Experience() {
       cargo: "Backend Developer",
       empresa: "NTT Data",
       tiempo: "06/2022 - 07/2022",
-      funciones: [],
+      funciones: [{ descripcion: "Desarrollé y mantuve procesos de base de datos utilizando SQL y Oracle OFSAA para optimizar la gestión financiera." } ],
       iconos: (
         <>
           <Tag text="SQL" className="dark:bg-l_orange bg-d_orange" />
@@ -22,9 +22,9 @@ function Experience() {
       empresa: "NTT Data",
       tiempo: "03/2023 - 06/2023",
       funciones: [
-        { descripcion: "Impulsé la evolución de aplicaciones, implementando nuevas funcionalidades y mejoras en las interfaces de usuario mediante Angular." },
-        { descripcion: "Utilicé Kiuwan para analizar y mejorar el código, abordando vulnerabilidades y errores, fortaleciendo la seguridad y eficiencia de las aplicaciones." },
-        { descripcion: "Utilicé Git para rastrear cambios en el código, creando y fusionando ramas para implementar funcionalidades y corregir errores, gestionando conflictos durante el proceso de fusión." },
+        { descripcion: "Desarrollo de nuevas funcionalidades y mejoras en interfaces de usuario con Angular" },
+        { descripcion: "Análisis y mejora de la calidad del código utilizando Kiuwan, corrigiendo vulnerabilidades y errores" },
+        { descripcion: "Gestión de versiones con Git, creación y fusión de ramas, y resolución de conflictos." },
       ],
       iconos: (
         <>
@@ -39,10 +39,10 @@ function Experience() {
       empresa: "NTT Data",
       tiempo: "07/2023 - 09/2024",
       funciones: [
-        { descripcion: "Creación de evolutivos utilizando una herramienta exclusiva proporcionada por el banco." },
-        { descripcion: "Competencia sólida en el desarrollo web con un enfoque particular en JavaScript." },
-        { descripcion: "Creación de Batch para la automatización de programas con el lenguaje Java." },
-        { descripcion: "Desarrollo e implementacion de APIs RESTful utilizando Java." },
+        { descripcion: "Desarrollé evolutivos bancarios con herramientas internas, agilizando procesos de negocio." },
+        { descripcion: "Colaboración activa con el equipo para optimizar procesos y prevenir incidencias futuras." },
+        { descripcion: "Creación de procesos Batch en Java para automatización de tareas." },
+        { descripcion: "Implementación y mantenimiento de APIs RESTful con Java y Spring Boot" },
       ],
       iconos: (
         <>
@@ -50,6 +50,7 @@ function Experience() {
           <Tag text="HTML" className="dark:bg-l_red bg-d_red" />
           <Tag text="Git" className="dark:bg-l_green bg-d_green" />
           <Tag text="Java" className="dark:bg-l_green bg-d_green" />
+          <Tag text="Spring Boot" className="dark:bg-l_blue bg-d_blue" />
         </>
       ),
     }
